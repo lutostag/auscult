c2bf365b06870f16ef0469cd3393803b067c2e87
Greg Lutostanski <greg.luto@gmail.com>
2014-11-11 00:22:01 -0600

    Add a way to send messages

--- auscult.go
+++ auscult.go
@@ -16,39 +16,98 @@ import (
     "crypto/cipher"
 )
 
-var listenAddr = flag.String("l", ":10600", "listen on the given ip:port")
+var address = flag.String("a", ":10600", "listen on/send to the given address (ip:port)")
 var passphrase = flag.String("p", "", "passphrase for crypto")
+var message = flag.String("m", "", "message to send, does not listen")
 const hashCount = int(10)
+const pkcsPad = "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
+var pkcsPadBytes = []byte(pkcsPad)
 
 func main() {
-    decrypting := false
+    crypting := false
+    sending := false
     flag.Parse()
     if *passphrase != "" {
-        decrypting = true
+        crypting = true
     }
-    server, err := net.Listen("tcp", *listenAddr)
-    defer server.Close()
-    if err != nil {
-        log.Fatal(err)
+    if *message != "" {
+        sending = true
     }
-    for {
-        connection, err := server.Accept()
+    if ! sending {
+        server, err := net.Listen("tcp", *address)
+        defer server.Close()
         if err != nil {
-            connection.Close()
             log.Fatal(err)
         }
-        go handleConn(connection, decrypting, *passphrase)
+        for {
+            connection, err := server.Accept()
+            if err != nil {
+                connection.Close()
+                log.Fatal(err)
+            }
+            go handleConn(connection, crypting, *passphrase)
+        }
+    } else {
+        conn, err := net.Dial("tcp", *address)
+        if err != nil {
+            log.Fatal(err)
+        }
+        scrubbedMessage := "///" + strings.Replace(*message, "/", "", -1) + "//"
+        messageBytes := []byte(scrubbedMessage)
+        if crypting {
+            messageBytes = encrypt(messageBytes, *passphrase)
+        }
+        conn.Write(messageBytes)
     }
 }
 
 func notify(status []byte){
     result := strings.SplitN(strings.TrimSuffix(string(status), "\x00"), "/", 6)
-    cmd := exec.Command("notify-send", strings.Join(result[3:], "\n"))
+    resultVal := ""
+    urgency := "--urgency=normal"
+    timeout := "--expire-time=5000"
+    if result[4] != "" || result [5] != "" {
+        resultVal = strings.Trim(strings.Join(result[3:], "\n"), " ")
+    } else {
+        urgency = "--urgency=critical"
+        resultVal = strings.Trim(string(result[3]), " ")
+    }
+    cmd := exec.Command("notify-send", urgency, timeout, resultVal)
     err := cmd.Run();
     if err != nil {
         fmt.Printf("[% x]", []byte(strings.Join(result[3:], "\n")))
         log.Fatal(err)
     }
+}
+
+func encrypt(plaintext []byte, passphrase string) []byte{
+    hash := md5.New()
+    io.WriteString(hash, passphrase)
+    iv := hash.Sum(nil)
+    key := iv
+    for index := 0; index < hashCount; index++ {
+        key = iv
+        hash = md5.New()
+        io.WriteString(hash, string(iv))
+        iv = hash.Sum(nil)
+    }
+
+    instance, err := aes.NewCipher(key)
+    if err != nil {
+        log.Fatal(err)
+    }
+    //wrong man's PKCS7 padding addition
+    if len(plaintext)%aes.BlockSize != 0 {
+        pad_length := aes.BlockSize - len(plaintext)%aes.BlockSize
+        plaintextPadded := make([]byte, len(plaintext)+pad_length) 
+        plaintextPadded = append(plaintext, pkcsPadBytes[:pad_length]...)
+        plaintext = plaintextPadded
+    }
+
+    ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+    encrypter := cipher.NewCBCEncrypter(instance, iv)
+    encrypter.CryptBlocks(ciphertext, plaintext)
+    return ciphertext[:len(plaintext)]
 }
 
 func decrypt(status []byte, passphrase string) []byte{
@@ -70,7 +129,7 @@ func decrypt(status []byte, passphrase string) []byte{
     decrypter := cipher.NewCBCDecrypter(instance, iv)
     decrypter.CryptBlocks(status, status)
     //poor man's PKCS7 padding removal
-    return bytes.TrimRight(status, "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f")
+    return bytes.TrimRight(status, pkcsPad)
 }
 
 func handleConn(connection net.Conn, decrypting bool, passphrase string) {
